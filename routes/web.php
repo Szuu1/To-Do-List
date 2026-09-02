1981ed2c9df94095970cbaf85cf67a78423d6184
Szuu1 <suanchristopherb220100815@gmail.com>
2026-09-02 18:42:05 +0800

    Adding store route inside web.php

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
