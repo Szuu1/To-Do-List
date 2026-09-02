7613c457637bbf1804342a394b47d6813614374d
Szuu1 <suanchristopherb220100815@gmail.com>
2026-09-02 18:51:15 +0800

    Adding delete route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\TaskController;
 
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
+Route::delete('/tasks/{task}', [TaskController::class, 'delete']);
